2643ceca2c137dc9699ccf707ded1c88d55270a9
Ilyass SHAIMOUN <36902604+ishaimou@users.noreply.github.com>
2023-12-04 00:19:19 +0100

    Revert "Update node version to 10"
    This reverts commit 727b724e75996ade752c053b57d71a1e51558688.

--- result/server.js
+++ result/server.js
@@ -1,8 +1,6 @@
 var express = require('express'),
     async = require('async'),
-    pg = require('pg'),
-    { Pool } = require('pg'),
-    path = require('path'),
+    pg = require("pg"),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
@@ -23,14 +21,10 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-var pool = new pg.Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
-});
-
 async.retry(
   {times: 1000, interval: 1000},
   function(callback) {
-    pool.connect(function(err, client, done) {
+    pg.connect('postgres://postgres_user:postgres_password@db/postgres', function(err, client, done) {
       if (err) {
         console.error("Waiting for db");
       }
@@ -39,7 +33,7 @@ async.retry(
   },
   function(err, client) {
     if (err) {
-      return console.error("Giving up");
+      return console.err("Giving up");
     }
     console.log("Connected to db");
     getVotes(client);
